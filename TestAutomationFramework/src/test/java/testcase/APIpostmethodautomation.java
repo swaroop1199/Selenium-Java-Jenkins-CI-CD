198202d05d8b2e96d5f0dcc50a0627ed1a183599
swaroop1199 <hiremathswaroop@gmail.com>
2024-11-15 11:50:19 +0530

    API Commit 5th

--- TestAutomationFramework/src/test/java/testcase/APIpostmethodautomation.java
+++ TestAutomationFramework/src/test/java/testcase/APIpostmethodautomation.java
@@ -2,25 +2,30 @@ package testcase;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+public class APIpostmethodautomation {
 
-public class APIautomation {
-
+	@Test(dataProvider="Userdata")
 	public static void main(String[] args) {
-		
 		RestAssured.baseURI="https://api.restful-api.dev";
 		RequestSpecification Rsp = RestAssured.given().log().all();
-		Response res = Rsp.request(Method.GET,"/objects?id=3");
+		Response res = Rsp.request(Method.POST,"/objects");
 		String resbody=res.getBody().asString();
 		int statuscode = res.statusCode();
 		Assert.assertEquals(statuscode, 200);
 		System.out.println(resbody);
 		System.out.println("success: "+statuscode);	
+
 	}
+
 }
+	
+
+
 
